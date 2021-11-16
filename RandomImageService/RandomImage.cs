660a7e1ab43f9f148c3e1d3f75dc79e6f4c50524
alicabuk <ali.cabuk@issd.com.tr>
2021-11-16 13:33:35 +0300

    initial commit

---
+++ RandomImageService/RandomImage.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Configuration;
+
+namespace RandomImageService
+{
+    public partial class RandomImage : ServiceBase
+    {
+        Timer timer;
+
+        Random random = new Random();
+        string alphabet = "ABCDEFGHIJKLMNOPRSTUVYZ";
+        string numbers = "0123456789";
+        char[] randomPlate = new char[8];
+        char[] vehicleId = new char[13];
+
+        string currentFileName;
+        string sourcePath;
+
+        string[] cameras = ConfigurationManager.AppSettings["Cameras"].Split(',');
+        string[] locationCodes = ConfigurationManager.AppSettings["LocationCodes"].Split(',');
+        public RandomImage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            timer = new Timer
+            {
+                Interval = 3000,
+                AutoReset = true
+            };
+            timer.Elapsed += FileNameByScenario;
+            timer.Start();
+        }
+
+        protected override void OnStop()
+        {
+            timer.Stop();
+            timer.Dispose();
+        }
+
+        public void CreateRandomPlate()
+        {
+            for (int i = 0; i < randomPlate.Length; i++)
+            {
+                if (i < 2)
+                {
+                    randomPlate[i] = numbers[random.Next(numbers.Length)];
+                }
+                if (i > 1 && i < 5)
+                {
+                    randomPlate[i] = alphabet[random.Next(alphabet.Length)];
+                }
+                if (i > 4 && i < 8)
+                {
+                    randomPlate[i] = numbers[random.Next(numbers.Length)];
+                }
+            }
+        }
+        public void CreateVehicleId()
+        {
+            for (int i = 0; i < vehicleId.Length; i++)
+            {
+                vehicleId[i] = numbers[random.Next(numbers.Length)];
+            }
+        }
+        public string CreateTime()
+        {
+            DateTime date = DateTime.Now;
+            date = date.AddMinutes(int.Parse(ConfigurationManager.AppSettings["MinuteDuration"]));
+            string time = date.ToString("yyyy-MM-dd-HH-mm-ss-fff");
+
+            return time;
+        }
+        public string CreatePlateLocation()
+        {
+            int branchSelection = int.Parse(ConfigurationManager.AppSettings["BranchSelection"]);
+            string plateLocation =string.Empty;
+            string colors = "0123456";
+            string types = "012";
+            string directions = "0123";
+
+            if (branchSelection == 1)
+            {
+                plateLocation = "973-920-1156-960";
+            }
+            if (branchSelection == 2)
+            {
+                plateLocation = string.Concat(colors[random.Next(colors.Length)]+"-"+ types[random.Next(types.Length)] + "-" + directions[random.Next(directions.Length)] + "-" + "0");
+            }
+
+            return plateLocation;
+        }
+        public string CreateFileNameAndMove(string destinationPath, string time, int locationIndex, int cameraIndex)
+        {
+            System.Threading.Thread.Sleep(int.Parse(ConfigurationManager.AppSettings["ImageRenderingTime"]));
+            string newPlate = new string(randomPlate);
+            string newVehicleId = new string(vehicleId);
+            string fileExtension = ConfigurationManager.AppSettings["FileExtension"];
+
+            var fileName = string.Concat(newPlate + "_" + cameras[cameraIndex] + "_" + newVehicleId + "_" + locationCodes[locationIndex] + "_" + time + "_" + CreatePlateLocation() + fileExtension);
+
+            string sourceDirectory = Path.Combine(sourcePath, fileName);
+            string destinationDirectory = Path.Combine(destinationPath, fileName);
+
+            File.Move(sourcePath + "\\" + currentFileName, sourcePath + "\\" + fileName);
+
+            if (Directory.Exists(destinationPath) == false)
+            {
+                Directory.CreateDirectory(destinationPath);
+            }
+
+            File.Copy(sourceDirectory, destinationDirectory);
+            currentFileName = fileName;
+
+            return fileName;
+        }
+        public void FileNameByScenario(object sender, ElapsedEventArgs e)
+        {
+            timer.Stop();
+            int scenario = int.Parse(ConfigurationManager.AppSettings["Scenario"]);
+            string destinationPath = ConfigurationManager.AppSettings["DestinationPath"];
+            string[] currentFile = Directory.GetFiles(ConfigurationManager.AppSettings["SourcePath"]);
+
+            foreach (var file in currentFile)
+            {
+                FileInfo fileInfo = new FileInfo(file);
+                sourcePath = fileInfo.DirectoryName;
+                currentFileName = fileInfo.Name;
+            }
+
+            //Scenario 1 - Location(BIOS ID) is different, Plate is different, Same Vehi"cleId(Unique)            
+            if (scenario == 1)
+            {
+                CreateVehicleId();
+                for (int cameraIndex = 0; cameraIndex < cameras.Length; cameraIndex++)
+                {
+                    CreateRandomPlate();
+                    int randomLocationIndex = random.Next(0, locationCodes.Length);
+                    CreateFileNameAndMove(destinationPath, CreateTime(), randomLocationIndex, cameraIndex);
+                }
+            }
+            //Scenario 2 - Location(BIOS ID) is different, Same Plate, VehicleId is different(Unique)
+            if (scenario == 2)
+            {
+                CreateRandomPlate();
+                for (int cameraIndex = 0; cameraIndex < cameras.Length; cameraIndex++)
+                {
+                    CreateVehicleId();
+                    int randomLocationIndex = random.Next(0, locationCodes.Length);
+                    CreateFileNameAndMove(destinationPath, CreateTime(), randomLocationIndex, cameraIndex);
+                }
+            }
+            timer.Start();
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.IO;
+using System.Timers;
+
+namespace Test
+{
+    class Program
+    {
+        static Timer timer;
+        static Random random = new Random();
+        static string currentFileName;
+        static string sourcePath;
+        static string alphabet = "ABCDEFGHIJKLMNOPRSTUVYZ";
+        static string numbers = "0123456789";
+        static char[] randomPlate = new char[8];
+        static char[] vehicleId = new char[7];
+        static string[] cameras = { "ON", "ARKA" };
+        static string[] locationCodes = { "080213011101", "090213011102", "100213011103" };
+        static string plateLocation = "973-920-1156-960";
+        static string fileExtension = ".jpg";
+        static int Scenario = 2;
+
+        static void Main(string[] args)
+        {
+            timer = new Timer {
+                Interval = 5000, AutoReset=true
+            };
+            timer.Elapsed += CreateFileName;
+            timer.Start();
+           
+            Console.ReadKey();
+        }
+        public static void CreateRandomPlate()
+        {
+            for (int i = 0; i < randomPlate.Length; i++)
+            {
+                if (i < 2)
+                {
+                    randomPlate[i] = numbers[random.Next(numbers.Length)];
+                }
+                if (i > 1 && i < 5)
+                {
+                    randomPlate[i] = alphabet[random.Next(alphabet.Length)];
+                }
+                if (i > 4 && i < 8)
+                {
+                    randomPlate[i] = numbers[random.Next(numbers.Length)];
+                }
+            }
+        }
+        public static void CreateVehicleId()
+        {
+            for (int i = 0; i < vehicleId.Length; i++)
+            {
+                vehicleId[i] = numbers[random.Next(numbers.Length)];
+            }
+        }
+        public static string CreateTime()
+        {
+            DateTime date = DateTime.Now;
+            date = date.AddMinutes(3);
+            string time = date.ToString("yyyy-MM-dd-HH-mm-ss-fff");
+            return time;
+        }
+        public static string MoveFile(string destinationPath, string time, int locationIndex, int cameraIndex)
+        {
+
+            System.Threading.Thread.Sleep(3000);
+            string newPlate = new string(randomPlate);
+            string newVehicleId = new string(vehicleId);
+
+            var fileName = string.Concat(newPlate + "_" + cameras[cameraIndex] + "_" + newVehicleId + "_" + locationCodes[locationIndex] + "_" + time + "_" + plateLocation + fileExtension);
+
+            string sourceDirectory = Path.Combine(sourcePath, fileName);
+            string destinationDirectory = Path.Combine(destinationPath, fileName);
+
+            File.Move(sourcePath + "\\" + currentFileName, sourcePath + "\\" + fileName, true);
+
+            if (Directory.Exists(destinationPath) == false)
+
+            {
+                Directory.CreateDirectory(destinationPath);
+            }
+
+            File.Copy(sourceDirectory, destinationDirectory);
+            currentFileName = fileName;
+            Console.WriteLine(fileName);
+            return fileName;
+        }
+        public static void CreateFileName(object sender, ElapsedEventArgs e)
+        {
+            timer.Stop();
+
+            string destinationPath = @"C:\Test\Save";
+            string[] currentFile = Directory.GetFiles(@"C:\Test\New");
+
+            //Get File Info
+            foreach (var file in currentFile)
+            {
+                FileInfo fileInfo = new FileInfo(file);
+                sourcePath = fileInfo.DirectoryName;
+                currentFileName = fileInfo.Name;
+            }
+
+            //Scenario 1 - Location is different, Plate is different, Same VehicleId            
+            if (Scenario == 1)
+            {
+                CreateVehicleId();
+                for (int cameraIndex = 0; cameraIndex < cameras.Length; cameraIndex++)
+                {
+                    CreateRandomPlate();
+                    int randomLocationIndex = random.Next(0, locationCodes.Length);
+                    MoveFile(destinationPath, CreateTime(), randomLocationIndex, cameraIndex);
+                }
+            }
+            //Scenario 2 - Location is different, Same Plate, VehicleId is different
+            if (Scenario == 2)
+            {
+                CreateRandomPlate();
+                for (int cameraIndex = 0; cameraIndex < cameras.Length; cameraIndex++)
+                {
+                    CreateVehicleId();
+                    int randomLocationIndex = random.Next(0,locationCodes.Length);
+                    MoveFile(destinationPath, CreateTime(), randomLocationIndex, cameraIndex) ;
+                }
+            }       
+            timer.Start();
+        }   
+
+    }
+}
